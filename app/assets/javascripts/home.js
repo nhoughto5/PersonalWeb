22322479fc057acfce50107c51dc4477ca1a925c
nhoughto5 <nhoughto@uvic.ca>
2016-12-28 15:16:55 -0800

    Bootstrap working, modal appearing

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,13 +1,3 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
-$(function ()
-{
-    $('.projectImg').on('click', function ()
-    {
-        $(this).width(1000);
-        var modal = document.getElementById('myModal');
-        var modalImg = document.getElementById("img01");
-        var captionText = document.getElementById("caption");
-    });
-});
