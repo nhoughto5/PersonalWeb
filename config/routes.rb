de597afef487834a5642a3a3af7fb48fa677b28c
nhoughto5 <nhoughto@uvic.ca>
2017-01-02 16:08:45 -0800

    Added find the donald and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get '/FindTheDonald', :to => redirect('/FindTheDonald.html')
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   match ':controller(/:action(/:id))', :via => :get
   root 'home#index'
 end
