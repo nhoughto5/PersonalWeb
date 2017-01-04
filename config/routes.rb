3976834bbdea22e919e15a88d92ec3593695aeba
nhoughto5 <nhoughto@uvic.ca>
2017-01-03 20:35:26 -0800

    Added music page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/FindTheDonald', :to => redirect('/FindTheDonald.html')
+  get '/Hootin', :to => redirect('/Hootin.html')
+  get '/TheMusic', :to => redirect('/TheMusic.html')
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match ':controller(/:action(/:id))', :via => :get
