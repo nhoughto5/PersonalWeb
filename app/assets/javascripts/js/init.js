cef571e87adae5d85ef1aa0f95d34e07c28c7789
nhoughto5 <nhoughto@uvic.ca>
2017-01-05 19:51:46 -0800

    Try this

--- app/assets/javascripts/js/init.js
+++ app/assets/javascripts/js/init.js
@@ -4,7 +4,7 @@
     Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 skel.init({
-	prefix: 'assets/style',
+	prefix: 'assets/css/style',
 	resetCSS: true,
 	boxModel: 'border',
 	grid: {
